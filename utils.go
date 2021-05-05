744c687d3721b8e5930e9f786f09122d990a0421
Juan Font Alonso <juanfontalonso@gmail.com>
2021-05-06 01:01:45 +0200

    Fixed linting issues

--- utils.go
+++ utils.go
@@ -21,6 +21,7 @@ import (
 	"tailscale.com/wgengine/wgcfg"
 )
 
+// Error is used to compare errors as per https://dave.cheney.net/2016/04/07/constant-errors
 type Error string
 
 func (e Error) Error() string { return string(e) }
