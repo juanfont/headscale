5a42bace67d1c00f2d5d4dcfcccddd14a320a804
Juan Font Alonso <juanfontalonso@gmail.com>
2021-05-18 23:33:08 +0200

    Use OS-independent paths for the abs method

--- cmd/headscale/cli/utils.go
+++ cmd/headscale/cli/utils.go
@@ -22,10 +22,10 @@ type ErrorOutput struct {
 func absPath(path string) string {
 	// If a relative path is provided, prefix it with the the directory where
 	// the config file was found.
-	if (path != "") && !strings.HasPrefix(path, "/") {
+	if (path != "") && !strings.HasPrefix(path, string(os.PathSeparator)) {
 		dir, _ := filepath.Split(viper.ConfigFileUsed())
 		if dir != "" {
-			path = dir + "/" + path
+			path = filepath.Join(dir, path)
 		}
 	}
 	return path
