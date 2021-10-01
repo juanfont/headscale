a10013d80bbbbd25daa5f74ebf5d2bb273a76a83
Josef Citrine <josef@citrine.dev>
2021-10-01 13:40:55 +0100

    Show subnet in key list

--- cmd/headscale/cli/preauthkeys.go
+++ cmd/headscale/cli/preauthkeys.go
@@ -58,7 +58,7 @@ var listPreAuthKeys = &cobra.Command{
 			return
 		}
 
-		d := pterm.TableData{{"ID", "Key", "Reusable", "Ephemeral", "Expiration", "Created"}}
+		d := pterm.TableData{{"ID", "Key", "Reusable", "Ephemeral", "Expiration", "Subnet", "Created"}}
 		for _, k := range *keys {
 			expiration := "-"
 			if k.Expiration != nil {
@@ -78,6 +78,7 @@ var listPreAuthKeys = &cobra.Command{
 				reusable,
 				strconv.FormatBool(k.Ephemeral),
 				expiration,
+				k.Subnet,
 				k.CreatedAt.Format("2006-01-02 15:04:05"),
 			})
 
