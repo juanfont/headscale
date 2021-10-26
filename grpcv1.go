caa4d33cbd1d3db42c21fd59ca72b3f469f41b3e
Kristoffer Dalby <kradalby@kradalby.no>
2021-10-26 20:42:20 +0000

    Add an initial grpcv1 service (implementing the proto generated service)

---
+++ grpcv1.go
@@ -0,0 +1,33 @@
+package headscale
+
+import (
+	"context"
+
+	apiV1 "github.com/juanfont/headscale/gen/go/v1"
+)
+
+type headscaleV1APIServer struct { // apiV1.HeadscaleServiceServer
+	apiV1.UnimplementedHeadscaleServiceServer
+	h *Headscale
+}
+
+func newHeadscaleV1APIServer(h *Headscale) apiV1.HeadscaleServiceServer {
+	return headscaleV1APIServer{
+		h: h,
+	}
+}
+
+func (api headscaleV1APIServer) GetMachine(
+	ctx context.Context,
+	request *apiV1.GetMachineRequest,
+) (*apiV1.Machine, error) {
+	m, err := api.h.GetMachineByID(request.MachineId)
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO(kradalby): Make this function actually do something
+	return &apiV1.Machine{Name: m.Name}, nil
+}
+
+func (api headscaleV1APIServer) mustEmbedUnimplementedHeadscaleServiceServer() {}
