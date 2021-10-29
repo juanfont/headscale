002b5c1dad9ba24baa4b54edab586c5f49136152
Kristoffer Dalby <kradalby@kradalby.no>
2021-10-29 17:08:21 +0000

    Add grpc token auth struct

--- cmd/headscale/cli/utils.go
+++ cmd/headscale/cli/utils.go
@@ -312,3 +312,18 @@ func HasJsonOutputFlag() bool {
 	}
 	return false
 }
+
+type tokenAuth struct {
+	token string
+}
+
+// Return value is mapped to request headers.
+func (t tokenAuth) GetRequestMetadata(ctx context.Context, in ...string) (map[string]string, error) {
+	return map[string]string{
+		"authorization": "Bearer " + t.token,
+	}, nil
+}
+
+func (tokenAuth) RequireTransportSecurity() bool {
+	return true
+}
