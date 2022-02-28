35616eb8611ab7514e45ab3a7eb01a5abe0c52f3
Kristoffer Dalby <kradalby@kradalby.no>
2022-02-28 16:41:28 +0000

    Fix oidc error were namespace isnt created #365

--- oidc.go
+++ oidc.go
@@ -17,7 +17,6 @@ import (
 	"github.com/patrickmn/go-cache"
 	"github.com/rs/zerolog/log"
 	"golang.org/x/oauth2"
-	"gorm.io/gorm"
 	"tailscale.com/types/key"
 )
 
@@ -297,7 +296,7 @@ func (h *Headscale) OIDCCallback(ctx *gin.Context) {
 		log.Debug().Msg("Registering new machine after successful callback")
 
 		namespace, err := h.GetNamespace(namespaceName)
-		if errors.Is(err, gorm.ErrRecordNotFound) {
+		if errors.Is(err, errNamespaceNotFound) {
 			namespace, err = h.CreateNamespace(namespaceName)
 
 			if err != nil {
