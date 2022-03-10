082a852c5e946fe7e58f0a94a332372d923fa7b5
Mevan Samaratunga <mevansam@gmail.com>
2022-03-10 10:40:20 -0500

    fixed linting recommendation

--- api.go
+++ api.go
@@ -601,7 +601,6 @@ func (h *Headscale) handleAuthKey(
 		machine.AuthKeyID = uint(pak.ID)
 		h.RefreshMachine(machine, registerRequest.Expiry)
 	} else {
-
 		now := time.Now().UTC()
 		machineToRegister := Machine{
 			Name:           registerRequest.Hostinfo.Hostname,
