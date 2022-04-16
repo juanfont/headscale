4fcc5e253c098f93df39a72c4c7ea8c4cf4f2a86
Adrien Raffin-Caboisse <adrien.raffin@sekoia.fr>
2022-04-16 13:15:18 +0200

    chore: fmt for grpc file

--- grpcv1.go
+++ grpcv1.go
@@ -281,7 +281,11 @@ func (api headscaleV1APIServer) ListMachines(
 	response := make([]*v1.Machine, len(machines))
 	for index, machine := range machines {
 		m := machine.toProto()
-		validTags, invalidTags := getTags(*api.h.aclPolicy, machine, api.h.cfg.OIDC.StripEmaildomain)
+		validTags, invalidTags := getTags(
+			*api.h.aclPolicy,
+			machine,
+			api.h.cfg.OIDC.StripEmaildomain,
+		)
 		m.InvalidTags = invalidTags
 		m.ValidTags = validTags
 		response[index] = m
