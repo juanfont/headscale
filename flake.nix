373db0dc5ec90299efe5a34a11838d53b4c06744
Juan Font Alonso <juanfontalonso@gmail.com>
2022-09-03 23:05:34 +0200

    Switch to nix unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "headscale - Open Source Tailscale Control server";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
