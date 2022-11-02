fffcf2a89c3d24cd8cb1b36872f1520cfe9ca32b
Liu Hancheng <cn_lhc@qq.com>
2022-11-02 12:44:03 +0800

    update the apple hint page

--- platform_config.go
+++ platform_config.go
@@ -142,7 +142,20 @@ func (h *Headscale) AppleConfigMessage(
 		<h1>headscale</h1>
 		<h2>Apple configuration profiles</h2>
 		<p>
-		    This page provides <a href="https://support.apple.com/guide/mdm/mdm-overview-mdmbf9e668/web">configuration profiles</a> for the official Tailscale clients for <a href="https://apps.apple.com/us/app/tailscale/id1470499037?ls=1">iOS</a> and <a href="https://apps.apple.com/ca/app/tailscale/id1475387142?mt=12">macOS</a>.
+		    This page provides <a href="https://support.apple.com/guide/mdm/mdm-overview-mdmbf9e668/web">configuration profiles</a> 
+			for the official Tailscale clients for
+			<ul>
+			<li>
+				<a href="https://apps.apple.com/us/app/tailscale/id1470499037?ls=1">iOS</a>
+			</li>
+			<li>
+				<a href="https://apps.apple.com/ca/app/tailscale/id1475387142?mt=12">macOS - AppStore Client</a>.
+			</li>
+			<li>
+				<a href="https://pkgs.tailscale.com/stable/#macos">macOS - Standalone Client</a>.
+			</li>
+
+			</ul>
 		</p>
 		<p>
 		    The profiles will configure Tailscale.app to use <code>{{.URL}}</code> as its control server.
@@ -167,19 +180,23 @@ func (h *Headscale) AppleConfigMessage(
 		<h3>macOS</h3>
 		<p>Headscale can be set to the default server by installing a Headscale configuration profile:</p>
 		<p>
-		    <a href="/apple/macos" download="headscale_macos.mobileconfig">macOS profile</a>
+		    <a href="/apple/macos-app-store" download="headscale_macos.mobileconfig">macOS AppStore profile</a>
+			<a href="/apple/macos-standalone" download="headscale_macos.mobileconfig">macOS Standalone profile</a>
 		</p>
 
 		<ol>
-		<li>Download the profile, then open it. When it has been opened, there should be a notification that a profile can be installed</li>
-		<li>Open System Preferences and go to "Profiles"</li>
-		<li>Find and install the Headscale profile</li>
-		<li>Restart Tailscale.app and log in</li>
+			<li>Download the profile, then open it. When it has been opened, there should be a notification that a profile can be installed</li>
+			<li>Open System Preferences and go to "Profiles"</li>
+			<li>Find and install the Headscale profile</li>
+			<li>Restart Tailscale.app and log in</li>
 		</ol>
 
 		<p>Or</p>
 		<p>Use your terminal to configure the default setting for Tailscale by issuing:</p>
-		<code>defaults write io.tailscale.ipn.macos ControlURL {{.URL}}</code>
+		<ul>
+			<li> for app store client: <code>defaults write io.tailscale.ipn.macos ControlURL {{.URL}}</code> </li>
+			<li> for standlone client: <code>defaults write io.tailscale.ipn.macsys ControlURL {{.URL}}</code> </li>
+		</ul>
 
 		<p>Restart Tailscale.app and log in.</p>
 
