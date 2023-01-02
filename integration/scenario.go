6de26b1d7c430be74369bf57c5976ea55f99092b
Even Holthe <even.holthe@bekk.no>
2023-01-02 15:01:29 +0100

    Remove Tailscale v1.18.2 from test matrix

--- integration/scenario.go
+++ integration/scenario.go
@@ -43,11 +43,11 @@ var (
 		"1.24.2",
 		"1.22.2",
 		"1.20.4",
-		"1.18.2",
 	}
 
 	// tailscaleVersionsUnavailable = []string{
 	// 	// These versions seem to fail when fetching from apt.
+	//  "1.18.2",
 	// 	"1.16.2",
 	// 	"1.14.6",
 	// 	"1.12.4",
