043be13e6d7c47d1bc17e7005c983f4d89a6a489
Juan Font <juanfontalonso@gmail.com>
2023-08-11 15:52:03 +0000

    Send logs to stderr, rather than stdout

--- cmd/headscale/headscale.go
+++ cmd/headscale/headscale.go
@@ -48,7 +48,7 @@ func main() {
 
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 	log.Logger = log.Output(zerolog.ConsoleWriter{
-		Out:        os.Stdout,
+		Out:        os.Stderr,
 		TimeFormat: time.RFC3339,
 		NoColor:    !colors,
 	})
