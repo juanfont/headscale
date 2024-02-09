a89eb6defa534de7b74dcbd48cadcab885a06adb
Kristoffer Dalby <kristoffer@tailscale.com>
2024-02-09 15:30:01 +0100

    add version comparer helper
    Signed-off-by: Kristoffer Dalby <kristoffer@tailscale.com>

---
+++ hscontrol/util/util.go
@@ -0,0 +1,14 @@
+package util
+
+import "tailscale.com/util/cmpver"
+
+func TailscaleVersionNewerOrEqual(minimum, toCheck string) bool {
+	if cmpver.Compare(minimum, toCheck) <= 0 ||
+		toCheck == "unstable" ||
+		toCheck == "head" {
+
+		return true
+	}
+
+	return false
+}
