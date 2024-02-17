b90c740cb44abba96f1065f0f8f889585e358ce3
Kristoffer Dalby <kristoffer@tailscale.com>
2024-02-17 13:50:26 +0100

    introduce dedicated ip allocator
    This new allocator keeps the used space in memory and
    loads the database only on startup. If it allocates but
    something errors before it gets saved to the database,
    then thats fine because it will be cleared on next restart.
    
    This should resolve races between ip allocation with postgres
    and as a side effect should make it easier to implement random
    allocation instead of serial if we so desire.
    
    Signed-off-by: Kristoffer Dalby <kristoffer@tailscale.com>

---
+++ hscontrol/db/ip.go
@@ -0,0 +1,145 @@
+package db
+
+import (
+	"fmt"
+	"net/netip"
+	"sync"
+
+	"github.com/juanfont/headscale/hscontrol/types"
+	"github.com/juanfont/headscale/hscontrol/util"
+	"go4.org/netipx"
+	"gorm.io/gorm"
+)
+
+// IPAllocator is a singleton responsible for allocating
+// IP addresses for nodes and making sure the same
+// address is not handed out twice. There can only be one
+// and it needs to be created before any other database
+// writes occur.
+type IPAllocator struct {
+	mu sync.Mutex
+
+	prefix4 netip.Prefix
+	prefix6 netip.Prefix
+
+	// Previous IPs handed out
+	prev4 netip.Addr
+	prev6 netip.Addr
+
+	// Set of all IPs handed out.
+	// This might not be in sync with the database,
+	// but it is more conservative. If saves to the
+	// database fails, the IP will be allocated here
+	// until the next restart of Headscale.
+	usedIPs netipx.IPSetBuilder
+}
+
+// NewIPAllocator returns a new IPAllocator singleton which
+// can be used to hand out unique IP addresses within the
+// provided IPv4 and IPv6 prefix. It needs to be created
+// when headscale starts and needs to finish its read
+// transaction before any writes to the database occur.
+func NewIPAllocator(db *HSDatabase, prefix4, prefix6 netip.Prefix) (*IPAllocator, error) {
+	var addressesSlices []string
+
+	if db != nil {
+		db.Read(func(rx *gorm.DB) error {
+			return rx.Model(&types.Node{}).Pluck("ip_addresses", &addressesSlices).Error
+		})
+	}
+
+	var ips netipx.IPSetBuilder
+
+	// Add network and broadcast addrs to used pool so they
+	// are not handed out to nodes.
+	network4, broadcast4 := util.GetIPPrefixEndpoints(prefix4)
+	network6, broadcast6 := util.GetIPPrefixEndpoints(prefix6)
+	ips.Add(network4)
+	ips.Add(broadcast4)
+	ips.Add(network6)
+	ips.Add(broadcast6)
+
+	// Fetch all the IP Addresses currently handed out from the Database
+	// and add them to the used IP set.
+	for _, slice := range addressesSlices {
+		var machineAddresses types.NodeAddresses
+		err := machineAddresses.Scan(slice)
+		if err != nil {
+			return nil, fmt.Errorf(
+				"parsing IPs from database %v: %w", machineAddresses,
+				err,
+			)
+		}
+
+		for _, ip := range machineAddresses {
+			ips.Add(ip)
+		}
+	}
+
+	// Build the initial IPSet to validate that we can use it.
+	_, err := ips.IPSet()
+	if err != nil {
+		return nil, fmt.Errorf(
+			"building initial IP Set: %w",
+			err,
+		)
+	}
+
+	return &IPAllocator{
+		usedIPs: ips,
+
+		prefix4: prefix4,
+		prefix6: prefix6,
+
+		// Use network as starting point, it will be used to call .Next()
+		// TODO(kradalby): Could potentially take all the IPs loaded from
+		// the database into account to start at a more "educated" location.
+		prev4: network4,
+		prev6: network6,
+	}, nil
+}
+
+func (i *IPAllocator) Next() (types.NodeAddresses, error) {
+	i.mu.Lock()
+	defer i.mu.Unlock()
+
+	v4, err := i.next(i.prev4, i.prefix4)
+	if err != nil {
+		return nil, fmt.Errorf("allocating IPv4 address: %w", err)
+	}
+
+	v6, err := i.next(i.prev6, i.prefix6)
+	if err != nil {
+		return nil, fmt.Errorf("allocating IPv6 address: %w", err)
+	}
+
+	return types.NodeAddresses{*v4, *v6}, nil
+}
+
+func (i *IPAllocator) next(prev netip.Addr, prefix netip.Prefix) (*netip.Addr, error) {
+	// Get the first IP in our prefix
+	ip := prev.Next()
+
+	// TODO(kradalby): maybe this can be done less often.
+	set, err := i.usedIPs.IPSet()
+	if err != nil {
+		return nil, err
+	}
+
+	for {
+		if !prefix.Contains(ip) {
+			return nil, ErrCouldNotAllocateIP
+		}
+
+		// Check if the IP has already been allocated.
+		if set.Contains(ip) {
+			ip = ip.Next()
+
+			continue
+		}
+
+		i.usedIPs.Add(ip)
+
+		return &ip, nil
+	}
+}
---
+++ hscontrol/db/ip_test.go
@@ -0,0 +1,152 @@
+package db
+
+import (
+	"net/netip"
+	"os"
+	"testing"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/google/go-cmp/cmp"
+	"github.com/juanfont/headscale/hscontrol/types"
+	"github.com/juanfont/headscale/hscontrol/util"
+)
+
+func TestIPAllocator(t *testing.T) {
+	mpp := func(pref string) netip.Prefix {
+		return netip.MustParsePrefix(pref)
+	}
+	na := func(pref string) netip.Addr {
+		return netip.MustParseAddr(pref)
+	}
+	newDb := func(p4, p6 netip.Prefix) *HSDatabase {
+		tmpDir, err := os.MkdirTemp("", "headscale-db-test-*")
+		if err != nil {
+			t.Fatalf("creating temp dir: %s", err)
+		}
+		db, _ = NewHeadscaleDatabase(
+			types.DatabaseConfig{
+				Type: "sqlite3",
+				Sqlite: types.SqliteConfig{
+					Path: tmpDir + "/headscale_test.db",
+				},
+			},
+			[]netip.Prefix{p4, p6},
+			"",
+		)
+
+		return db
+	}
+
+	tests := []struct {
+		name   string
+		dbFunc func() *HSDatabase
+
+		prefix4  netip.Prefix
+		prefix6  netip.Prefix
+		getCount int
+		want     []types.NodeAddresses
+	}{
+		{
+			name: "simple",
+			dbFunc: func() *HSDatabase {
+				return nil
+			},
+
+			prefix4: mpp("100.64.0.0/10"),
+			prefix6: mpp("fd7a:115c:a1e0::/48"),
+
+			getCount: 1,
+
+			want: []types.NodeAddresses{
+				{
+					na("100.64.0.1"),
+					na("fd7a:115c:a1e0::1"),
+				},
+			},
+		},
+		{
+			name: "simple-with-db",
+			dbFunc: func() *HSDatabase {
+				db := newDb(mpp("100.64.0.0/10"), mpp("fd7a:115c:a1e0::/48"))
+
+				db.DB.Save(&types.Node{
+					IPAddresses: types.NodeAddresses{
+						na("100.64.0.1"),
+						na("fd7a:115c:a1e0::1"),
+					},
+				})
+
+				return db
+			},
+
+			prefix4: mpp("100.64.0.0/10"),
+			prefix6: mpp("fd7a:115c:a1e0::/48"),
+
+			getCount: 1,
+
+			want: []types.NodeAddresses{
+				{
+					na("100.64.0.2"),
+					na("fd7a:115c:a1e0::2"),
+				},
+			},
+		},
+		{
+			name: "before-after-free-middle-in-db",
+			dbFunc: func() *HSDatabase {
+				db := newDb(mpp("100.64.0.0/10"), mpp("fd7a:115c:a1e0::/48"))
+
+				db.DB.Save(&types.Node{
+					IPAddresses: types.NodeAddresses{
+						na("100.64.0.2"),
+						na("fd7a:115c:a1e0::2"),
+					},
+				})
+
+				return db
+			},
+
+			prefix4: mpp("100.64.0.0/10"),
+			prefix6: mpp("fd7a:115c:a1e0::/48"),
+
+			getCount: 2,
+
+			want: []types.NodeAddresses{
+				{
+					na("100.64.0.1"),
+					na("fd7a:115c:a1e0::1"),
+				},
+				{
+					na("100.64.0.3"),
+					na("fd7a:115c:a1e0::3"),
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			db := tt.dbFunc()
+
+			alloc, _ := NewIPAllocator(db, tt.prefix4, tt.prefix6)
+
+			spew.Dump(alloc)
+
+			t.Logf("prefixes: %q, %q", tt.prefix4.String(), tt.prefix6.String())
+
+			var got []types.NodeAddresses
+
+			for range tt.getCount {
+				gotSet, err := alloc.Next()
+				if err != nil {
+					t.Fatalf("allocating next IP: %s", err)
+				}
+
+				got = append(got, gotSet)
+			}
+			if diff := cmp.Diff(tt.want, got, util.Comparers...); diff != "" {
+				t.Errorf("IPAllocator unexpected result (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
