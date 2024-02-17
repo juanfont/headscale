9116cf23e0ffba2d139fbdbf46c89777edfbb762
Kristoffer Dalby <kristoffer@tailscale.com>
2024-02-17 14:05:00 +0100

    require go 1.22
    Signed-off-by: Kristoffer Dalby <kristoffer@tailscale.com>

--- flake.nix
+++ flake.nix
@@ -31,7 +31,7 @@
 
           # When updating go.mod or go.sum, a new sha will need to be calculated,
           # update this if you have a mismatch after doing a change to thos files.
-          vendorHash = "sha256-Ko47U0nOJIacXTbw3rpUGlnmDvr3CWM8LttIoG5Pppk=";
+          vendorHash = "sha256-Yb5WaN0abPLZ4mPnuJGZoj6EMfoZjaZZ0f344KWva3o=";
 
           subPackages = ["cmd/headscale"];
 
