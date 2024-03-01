cd4afbbadc6836cc0ccbf0c0b38e86513742dc36
Kristoffer Dalby <kristoffer@tailscale.com>
2024-03-01 13:14:36 +0100

    run assert state twice in ping test
    Signed-off-by: Kristoffer Dalby <kristoffer@tailscale.com>

--- integration/general_test.go
+++ integration/general_test.go
@@ -66,6 +66,7 @@ func TestPingAllByIP(t *testing.T) {
 	assertNoErrSync(t, err)
 
 	assertClientsState(t, allClients)
+	t.Log("-------------------------------------------------------------------------")
 
 	allAddrs := lo.Map(allIps, func(x netip.Addr, index int) string {
 		return x.String()
@@ -73,6 +74,8 @@ func TestPingAllByIP(t *testing.T) {
 
 	success := pingAllHelper(t, allClients, allAddrs)
 	t.Logf("%d successful pings out of %d", success, len(allClients)*len(allIps))
+	t.Log("\n\n-------------------------------------------------------------------------")
+	assertClientsState(t, allClients)
 }
 
 func TestPingAllByIPPublicDERP(t *testing.T) {
