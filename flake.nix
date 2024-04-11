2858ab402a347f951b8b3a438c62b49aca8b1d87
Brandon Schneider <github@bschneider.email>
2024-04-10 19:03:05 -0500

    Add new shasum for flake.nix

--- flake.nix
+++ flake.nix
@@ -31,7 +31,7 @@
 
           # When updating go.mod or go.sum, a new sha will need to be calculated,
           # update this if you have a mismatch after doing a change to thos files.
-          vendorHash = "sha256-Yb5WaN0abPLZ4mPnuJGZoj6EMfoZjaZZ0f344KWva3o=";
+          vendorHash = "sha256-z3IXmr8SK8oUJTnw7gTok6zpLf15kE89q6zYKbMA5AI=";
 
           subPackages = ["cmd/headscale"];
 
