f317a85ab4351c376468d9b420cb95cddbdf1067
Kristoffer Dalby <kristoffer@dalby.cc>
2025-05-03 17:36:08 +0300

    go.mod: update rest of deps (#2559)
    * flake: update go hash
    
    Signed-off-by: Kristoffer Dalby <kristoffer@tailscale.com>
    
    * go.mod: update more deps
    
    Signed-off-by: Kristoffer Dalby <kristoffer@tailscale.com>
    
    ---------
    
    Signed-off-by: Kristoffer Dalby <kristoffer@tailscale.com>

--- flake.nix
+++ flake.nix
@@ -30,7 +30,7 @@
 
           # When updating go.mod or go.sum, a new sha will need to be calculated,
           # update this if you have a mismatch after doing a change to those files.
-          vendorHash = "sha256-CoxqEAxGdefyiIhz84LXXxPrZ1JWsX8Ernv1USr9JTs=";
+          vendorHash = "sha256-dR8xmUIDMIy08lhm7r95GNNMAbXv4qSH3v9HR40HlNk=";
 
           subPackages = ["cmd/headscale"];
 
