6d9ad7fe5def7cc9ed8cadd2585fc914f351839e
Andrey Bobelev <andrey@bobelev.com>
2025-05-14 15:04:31 +0300

    chore(derp): prioritize loading DERP maps from URLs
    This allows users to override default entries provided via URL

--- hscontrol/derp/derp.go
+++ hscontrol/derp/derp.go
@@ -86,25 +86,6 @@ func GetDERPMap(cfg types.DERPConfig) *tailcfg.DERPMap {
 		derpMaps = append(derpMaps, cfg.DERPMap)
 	}
 
-	for _, path := range cfg.Paths {
-		log.Debug().
-			Str("func", "GetDERPMap").
-			Str("path", path).
-			Msg("Loading DERPMap from path")
-		derpMap, err := loadDERPMapFromPath(path)
-		if err != nil {
-			log.Error().
-				Str("func", "GetDERPMap").
-				Str("path", path).
-				Err(err).
-				Msg("Could not load DERP map from path")
-
-			break
-		}
-
-		derpMaps = append(derpMaps, derpMap)
-	}
-
 	for _, addr := range cfg.URLs {
 		derpMap, err := loadDERPMapFromURL(addr)
 		log.Debug().
@@ -115,6 +96,25 @@ func GetDERPMap(cfg types.DERPConfig) *tailcfg.DERPMap {
 			log.Error().
 				Str("func", "GetDERPMap").
 				Str("url", addr.String()).
+				Err(err).
+				Msg("Could not load DERP map from path")
+
+			break
+		}
+
+		derpMaps = append(derpMaps, derpMap)
+	}
+
+	for _, path := range cfg.Paths {
+		log.Debug().
+			Str("func", "GetDERPMap").
+			Str("path", path).
+			Msg("Loading DERPMap from path")
+		derpMap, err := loadDERPMapFromPath(path)
+		if err != nil {
+			log.Error().
+				Str("func", "GetDERPMap").
+				Str("path", path).
 				Err(err).
 				Msg("Could not load DERP map from path")
 
