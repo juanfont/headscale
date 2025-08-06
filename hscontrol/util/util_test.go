81b3e8f74397b91de98421341ce85ac4fecea98b
Kristoffer Dalby <kristoffer@tailscale.com>
2025-08-06 08:44:59 +0200

    util: harden parsing of traceroute
    Signed-off-by: Kristoffer Dalby <kristoffer@tailscale.com>

--- hscontrol/util/util_test.go
+++ hscontrol/util/util_test.go
@@ -335,6 +335,431 @@ func TestParseTraceroute(t *testing.T) {
 			want:    Traceroute{},
 			wantErr: true,
 		},
+		{
+			name: "windows tracert format",
+			input: `Tracing route to google.com [8.8.8.8]
+over a maximum of 30 hops:
+
+  1    <1 ms    <1 ms    <1 ms  router.local [192.168.1.1]
+  2     5 ms     4 ms     5 ms  10.0.0.1
+  3     *        *        *     Request timed out.
+  4    20 ms    19 ms    21 ms  8.8.8.8`,
+			want: Traceroute{
+				Hostname: "google.com",
+				IP:       netip.MustParseAddr("8.8.8.8"),
+				Route: []TraceroutePath{
+					{
+						Hop:      1,
+						Hostname: "router.local",
+						IP:       netip.MustParseAddr("192.168.1.1"),
+						Latencies: []time.Duration{
+							1 * time.Millisecond,
+							1 * time.Millisecond,
+							1 * time.Millisecond,
+						},
+					},
+					{
+						Hop:      2,
+						Hostname: "10.0.0.1",
+						IP:       netip.MustParseAddr("10.0.0.1"),
+						Latencies: []time.Duration{
+							5 * time.Millisecond,
+							4 * time.Millisecond,
+							5 * time.Millisecond,
+						},
+					},
+					{
+						Hop:      3,
+						Hostname: "*",
+					},
+					{
+						Hop:      4,
+						Hostname: "8.8.8.8",
+						IP:       netip.MustParseAddr("8.8.8.8"),
+						Latencies: []time.Duration{
+							20 * time.Millisecond,
+							19 * time.Millisecond,
+							21 * time.Millisecond,
+						},
+					},
+				},
+				Success: true,
+				Err:     nil,
+			},
+			wantErr: false,
+		},
+		{
+			name: "mixed latency formats",
+			input: `traceroute to 192.168.1.1 (192.168.1.1), 30 hops max, 60 byte packets
+ 1  gateway (192.168.1.1)  0.5 ms  *  0.4 ms`,
+			want: Traceroute{
+				Hostname: "192.168.1.1",
+				IP:       netip.MustParseAddr("192.168.1.1"),
+				Route: []TraceroutePath{
+					{
+						Hop:      1,
+						Hostname: "gateway",
+						IP:       netip.MustParseAddr("192.168.1.1"),
+						Latencies: []time.Duration{
+							500 * time.Microsecond,
+							400 * time.Microsecond,
+						},
+					},
+				},
+				Success: true,
+				Err:     nil,
+			},
+			wantErr: false,
+		},
+		{
+			name: "only one latency value",
+			input: `traceroute to 10.0.0.1 (10.0.0.1), 30 hops max, 60 byte packets
+ 1  10.0.0.1 (10.0.0.1)  1.5 ms`,
+			want: Traceroute{
+				Hostname: "10.0.0.1",
+				IP:       netip.MustParseAddr("10.0.0.1"),
+				Route: []TraceroutePath{
+					{
+						Hop:      1,
+						Hostname: "10.0.0.1",
+						IP:       netip.MustParseAddr("10.0.0.1"),
+						Latencies: []time.Duration{
+							1500 * time.Microsecond,
+						},
+					},
+				},
+				Success: true,
+				Err:     nil,
+			},
+			wantErr: false,
+		},
+		{
+			name: "backward compatibility - original format with 3 latencies",
+			input: `traceroute to 172.24.0.3 (172.24.0.3), 30 hops max, 46 byte packets
+ 1  ts-head-hk0urr.headscale.net (100.64.0.1)  1.135 ms  0.922 ms  0.619 ms
+ 2  172.24.0.3 (172.24.0.3)  0.593 ms  0.549 ms  0.522 ms`,
+			want: Traceroute{
+				Hostname: "172.24.0.3",
+				IP:       netip.MustParseAddr("172.24.0.3"),
+				Route: []TraceroutePath{
+					{
+						Hop:      1,
+						Hostname: "ts-head-hk0urr.headscale.net",
+						IP:       netip.MustParseAddr("100.64.0.1"),
+						Latencies: []time.Duration{
+							1135 * time.Microsecond,
+							922 * time.Microsecond,
+							619 * time.Microsecond,
+						},
+					},
+					{
+						Hop:      2,
+						Hostname: "172.24.0.3",
+						IP:       netip.MustParseAddr("172.24.0.3"),
+						Latencies: []time.Duration{
+							593 * time.Microsecond,
+							549 * time.Microsecond,
+							522 * time.Microsecond,
+						},
+					},
+				},
+				Success: true,
+				Err:     nil,
+			},
+			wantErr: false,
+		},
+		{
+			name: "two latencies only - common on packet loss",
+			input: `traceroute to 8.8.8.8 (8.8.8.8), 30 hops max, 60 byte packets
+ 1  gateway (192.168.1.1)  1.2 ms  1.1 ms`,
+			want: Traceroute{
+				Hostname: "8.8.8.8",
+				IP:       netip.MustParseAddr("8.8.8.8"),
+				Route: []TraceroutePath{
+					{
+						Hop:      1,
+						Hostname: "gateway",
+						IP:       netip.MustParseAddr("192.168.1.1"),
+						Latencies: []time.Duration{
+							1200 * time.Microsecond,
+							1100 * time.Microsecond,
+						},
+					},
+				},
+				Success: false,
+				Err:     errors.New("traceroute did not reach target"),
+			},
+			wantErr: false,
+		},
+		{
+			name: "hostname without parentheses - some traceroute versions",
+			input: `traceroute to 8.8.8.8 (8.8.8.8), 30 hops max, 60 byte packets
+ 1  192.168.1.1  1.2 ms  1.1 ms  1.0 ms
+ 2  8.8.8.8  20.1 ms  19.9 ms  20.2 ms`,
+			want: Traceroute{
+				Hostname: "8.8.8.8",
+				IP:       netip.MustParseAddr("8.8.8.8"),
+				Route: []TraceroutePath{
+					{
+						Hop:      1,
+						Hostname: "192.168.1.1",
+						IP:       netip.MustParseAddr("192.168.1.1"),
+						Latencies: []time.Duration{
+							1200 * time.Microsecond,
+							1100 * time.Microsecond,
+							1000 * time.Microsecond,
+						},
+					},
+					{
+						Hop:      2,
+						Hostname: "8.8.8.8",
+						IP:       netip.MustParseAddr("8.8.8.8"),
+						Latencies: []time.Duration{
+							20100 * time.Microsecond,
+							19900 * time.Microsecond,
+							20200 * time.Microsecond,
+						},
+					},
+				},
+				Success: true,
+				Err:     nil,
+			},
+			wantErr: false,
+		},
+		{
+			name: "ipv6 traceroute",
+			input: `traceroute to 2001:4860:4860::8888 (2001:4860:4860::8888), 30 hops max, 80 byte packets
+ 1  2001:db8::1 (2001:db8::1)  1.123 ms  1.045 ms  0.987 ms
+ 2  2001:4860:4860::8888 (2001:4860:4860::8888)  15.234 ms  14.876 ms  15.123 ms`,
+			want: Traceroute{
+				Hostname: "2001:4860:4860::8888",
+				IP:       netip.MustParseAddr("2001:4860:4860::8888"),
+				Route: []TraceroutePath{
+					{
+						Hop:      1,
+						Hostname: "2001:db8::1",
+						IP:       netip.MustParseAddr("2001:db8::1"),
+						Latencies: []time.Duration{
+							1123 * time.Microsecond,
+							1045 * time.Microsecond,
+							987 * time.Microsecond,
+						},
+					},
+					{
+						Hop:      2,
+						Hostname: "2001:4860:4860::8888",
+						IP:       netip.MustParseAddr("2001:4860:4860::8888"),
+						Latencies: []time.Duration{
+							15234 * time.Microsecond,
+							14876 * time.Microsecond,
+							15123 * time.Microsecond,
+						},
+					},
+				},
+				Success: true,
+				Err:     nil,
+			},
+			wantErr: false,
+		},
+		{
+			name: "macos traceroute with extra spacing",
+			input: `traceroute to google.com (8.8.8.8), 64 hops max, 52 byte packets
+ 1  router.home (192.168.1.1)   2.345 ms   1.234 ms   1.567 ms
+ 2  * * *
+ 3  isp-gw.net (10.1.1.1)   15.234 ms   14.567 ms   15.890 ms
+ 4  google.com (8.8.8.8)   20.123 ms   19.456 ms   20.789 ms`,
+			want: Traceroute{
+				Hostname: "google.com",
+				IP:       netip.MustParseAddr("8.8.8.8"),
+				Route: []TraceroutePath{
+					{
+						Hop:      1,
+						Hostname: "router.home",
+						IP:       netip.MustParseAddr("192.168.1.1"),
+						Latencies: []time.Duration{
+							2345 * time.Microsecond,
+							1234 * time.Microsecond,
+							1567 * time.Microsecond,
+						},
+					},
+					{
+						Hop:      2,
+						Hostname: "*",
+					},
+					{
+						Hop:      3,
+						Hostname: "isp-gw.net",
+						IP:       netip.MustParseAddr("10.1.1.1"),
+						Latencies: []time.Duration{
+							15234 * time.Microsecond,
+							14567 * time.Microsecond,
+							15890 * time.Microsecond,
+						},
+					},
+					{
+						Hop:      4,
+						Hostname: "google.com",
+						IP:       netip.MustParseAddr("8.8.8.8"),
+						Latencies: []time.Duration{
+							20123 * time.Microsecond,
+							19456 * time.Microsecond,
+							20789 * time.Microsecond,
+						},
+					},
+				},
+				Success: true,
+				Err:     nil,
+			},
+			wantErr: false,
+		},
+		{
+			name: "busybox traceroute minimal format",
+			input: `traceroute to 10.0.0.1 (10.0.0.1), 30 hops max, 38 byte packets
+ 1  10.0.0.1 (10.0.0.1)  1.234 ms  1.123 ms  1.456 ms`,
+			want: Traceroute{
+				Hostname: "10.0.0.1",
+				IP:       netip.MustParseAddr("10.0.0.1"),
+				Route: []TraceroutePath{
+					{
+						Hop:      1,
+						Hostname: "10.0.0.1",
+						IP:       netip.MustParseAddr("10.0.0.1"),
+						Latencies: []time.Duration{
+							1234 * time.Microsecond,
+							1123 * time.Microsecond,
+							1456 * time.Microsecond,
+						},
+					},
+				},
+				Success: true,
+				Err:     nil,
+			},
+			wantErr: false,
+		},
+		{
+			name: "linux traceroute with dns failure fallback to IP",
+			input: `traceroute to example.com (93.184.216.34), 30 hops max, 60 byte packets
+ 1  192.168.1.1 (192.168.1.1)  1.234 ms  1.123 ms  1.098 ms
+ 2  10.0.0.1 (10.0.0.1)  5.678 ms  5.432 ms  5.321 ms
+ 3  93.184.216.34 (93.184.216.34)  20.123 ms  19.876 ms  20.234 ms`,
+			want: Traceroute{
+				Hostname: "example.com",
+				IP:       netip.MustParseAddr("93.184.216.34"),
+				Route: []TraceroutePath{
+					{
+						Hop:      1,
+						Hostname: "192.168.1.1",
+						IP:       netip.MustParseAddr("192.168.1.1"),
+						Latencies: []time.Duration{
+							1234 * time.Microsecond,
+							1123 * time.Microsecond,
+							1098 * time.Microsecond,
+						},
+					},
+					{
+						Hop:      2,
+						Hostname: "10.0.0.1",
+						IP:       netip.MustParseAddr("10.0.0.1"),
+						Latencies: []time.Duration{
+							5678 * time.Microsecond,
+							5432 * time.Microsecond,
+							5321 * time.Microsecond,
+						},
+					},
+					{
+						Hop:      3,
+						Hostname: "93.184.216.34",
+						IP:       netip.MustParseAddr("93.184.216.34"),
+						Latencies: []time.Duration{
+							20123 * time.Microsecond,
+							19876 * time.Microsecond,
+							20234 * time.Microsecond,
+						},
+					},
+				},
+				Success: true,
+				Err:     nil,
+			},
+			wantErr: false,
+		},
+		{
+			name: "alpine linux traceroute with ms variations",
+			input: `traceroute to 1.1.1.1 (1.1.1.1), 30 hops max, 46 byte packets
+ 1  gateway (192.168.0.1)  0.456ms  0.389ms  0.412ms
+ 2  1.1.1.1 (1.1.1.1)  8.234ms  7.987ms  8.123ms`,
+			want: Traceroute{
+				Hostname: "1.1.1.1",
+				IP:       netip.MustParseAddr("1.1.1.1"),
+				Route: []TraceroutePath{
+					{
+						Hop:      1,
+						Hostname: "gateway",
+						IP:       netip.MustParseAddr("192.168.0.1"),
+						Latencies: []time.Duration{
+							456 * time.Microsecond,
+							389 * time.Microsecond,
+							412 * time.Microsecond,
+						},
+					},
+					{
+						Hop:      2,
+						Hostname: "1.1.1.1",
+						IP:       netip.MustParseAddr("1.1.1.1"),
+						Latencies: []time.Duration{
+							8234 * time.Microsecond,
+							7987 * time.Microsecond,
+							8123 * time.Microsecond,
+						},
+					},
+				},
+				Success: true,
+				Err:     nil,
+			},
+			wantErr: false,
+		},
+		{
+			name: "mixed asterisk and latency values",
+			input: `traceroute to 8.8.8.8 (8.8.8.8), 30 hops max, 60 byte packets
+ 1  gateway (192.168.1.1)  *  1.234 ms  1.123 ms
+ 2  10.0.0.1 (10.0.0.1)  5.678 ms  *  5.432 ms
+ 3  8.8.8.8 (8.8.8.8)  20.123 ms  19.876 ms  *`,
+			want: Traceroute{
+				Hostname: "8.8.8.8",
+				IP:       netip.MustParseAddr("8.8.8.8"),
+				Route: []TraceroutePath{
+					{
+						Hop:      1,
+						Hostname: "gateway",
+						IP:       netip.MustParseAddr("192.168.1.1"),
+						Latencies: []time.Duration{
+							1234 * time.Microsecond,
+							1123 * time.Microsecond,
+						},
+					},
+					{
+						Hop:      2,
+						Hostname: "10.0.0.1",
+						IP:       netip.MustParseAddr("10.0.0.1"),
+						Latencies: []time.Duration{
+							5678 * time.Microsecond,
+							5432 * time.Microsecond,
+						},
+					},
+					{
+						Hop:      3,
+						Hostname: "8.8.8.8",
+						IP:       netip.MustParseAddr("8.8.8.8"),
+						Latencies: []time.Duration{
+							20123 * time.Microsecond,
+							19876 * time.Microsecond,
+						},
+					},
+				},
+				Success: true,
+				Err:     nil,
+			},
+			wantErr: false,
+		},
 	}
 
 	for _, tt := range tests {
