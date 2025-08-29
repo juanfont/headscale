a7096a3fcc6b4208c1f2a01195c23f81a7fedb2c
Andrey Bobelev <andrey@bobelev.com>
2025-08-29 15:55:42 +0200

    fix: return valid AuthUrl in followup request on expired reg id
    - tailscale client gets a new AuthUrl and sets entry in the regcache
    - regcache entry expires
    - client doesn't know about that
    - client always polls followup request а gets error
    
    When user clicks "Login" in the app (after cache expiry), they visit
    invalid URL and get "node not found in registration cache". Some clients
    on Windows for e.g. can't get a new AuthUrl without restart the app.
    
    To fix that we can issue a new reg id and return user a new valid
    AuthUrl.
    
    RegisterNode is refactored to be created with NewRegisterNode() to
    autocreate channel and other stuff.

--- hscontrol/state/state.go
+++ hscontrol/state/state.go
@@ -90,6 +90,12 @@ func NewState(cfg *types.Config) (*State, error) {
 		cacheCleanup,
 	)
 
+	registrationCache.OnEvicted(
+		func(id types.RegistrationID, rn types.RegisterNode) {
+			rn.SendAndClose(nil)
+		},
+	)
+
 	db, err := hsdb.NewHeadscaleDatabase(
 		cfg.Database,
 		cfg.BaseDomain,
@@ -1286,15 +1292,11 @@ func (s *State) HandleNodeFromAuthPath(
 		s.nodeStore.PutNode(*savedNode)
 	}
 
+	// Signal to waiting clients
+	regEntry.SendAndClose(savedNode)
+
 	// Delete from registration cache
 	s.registrationCache.Delete(registrationID)
-
-	// Signal to waiting clients
-	select {
-	case regEntry.Registered <- savedNode:
-	default:
-	}
-	close(regEntry.Registered)
 
 	// Update policy manager
 	nodesChange, err := s.updatePolicyManagerNodes()
