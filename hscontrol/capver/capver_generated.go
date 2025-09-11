54d24d5dcbeb0c3ff610967f418b101cef7842c1
Kristoffer Dalby <kristoffer@tailscale.com>
2025-09-11 14:21:53 +0200

    capver: generate
    Signed-off-by: Kristoffer Dalby <kristoffer@tailscale.com>

--- hscontrol/capver/capver_generated.go
+++ hscontrol/capver/capver_generated.go
@@ -5,11 +5,6 @@ package capver
 import "tailscale.com/tailcfg"
 
 var tailscaleToCapVer = map[string]tailcfg.CapabilityVersion{
-	"v1.64.0": 90,
-	"v1.64.1": 90,
-	"v1.64.2": 90,
-	"v1.66.0": 95,
-	"v1.66.1": 95,
 	"v1.66.2": 95,
 	"v1.66.3": 95,
 	"v1.66.4": 95,
@@ -35,11 +30,15 @@ var tailscaleToCapVer = map[string]tailcfg.CapabilityVersion{
 	"v1.84.0": 116,
 	"v1.84.1": 116,
 	"v1.84.2": 116,
+	"v1.86.0": 122,
+	"v1.86.2": 123,
+	"v1.88.1": 125,
+	"v1.88.3": 125,
+	"v1.90.1": 130,
 }
 
 var capVerToTailscaleVer = map[tailcfg.CapabilityVersion]string{
-	90:  "v1.64.0",
-	95:  "v1.66.0",
+	95:  "v1.66.2",
 	97:  "v1.68.0",
 	102: "v1.70.0",
 	104: "v1.72.0",
@@ -48,4 +47,12 @@ var capVerToTailscaleVer = map[tailcfg.CapabilityVersion]string{
 	113: "v1.80.0",
 	115: "v1.82.0",
 	116: "v1.84.0",
+	122: "v1.86.0",
+	123: "v1.86.2",
+	125: "v1.88.1",
+	130: "v1.90.1",
 }
+
+// MinSupportedCapabilityVersion represents the minimum capability version
+// supported by this Headscale instance (latest 10 minor versions)
+const MinSupportedCapabilityVersion tailcfg.CapabilityVersion = 104
