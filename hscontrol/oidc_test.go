7a78fcd2db1b58b67fb8ed36e4e638514f5cbe05
Justin Angel <justinangel86@gmail.com>
2025-12-12 11:49:41 -0500

    Change for OIDC unverified email and testing
    Authorization functions are adjusted to allow
    tests while introducing logic to handle unverified
    email addresses.

---
+++ hscontrol/oidc_test.go
@@ -0,0 +1,133 @@
+package hscontrol
+
+import (
+	"testing"
+
+	"github.com/juanfont/headscale/hscontrol/types"
+)
+
+func TestDoOIDCAuthorization(t *testing.T) {
+	testCases := []struct {
+		name    string
+		cfg     *types.OIDCConfig
+		claims  *types.OIDCClaims
+		wantErr bool
+	}{
+		{
+			name:    "verified email domain",
+			wantErr: false,
+			cfg: &types.OIDCConfig{
+				EmailVerifiedRequired: true,
+				AllowedDomains:        []string{"test.com"},
+				AllowedUsers:          []string{},
+				AllowedGroups:         []string{},
+			},
+			claims: &types.OIDCClaims{
+				Email:         "user@test.com",
+				EmailVerified: true,
+			},
+		},
+		{
+			name:    "verified email user",
+			wantErr: false,
+			cfg: &types.OIDCConfig{
+				EmailVerifiedRequired: true,
+				AllowedDomains:        []string{},
+				AllowedUsers:          []string{"user@test.com"},
+				AllowedGroups:         []string{},
+			},
+			claims: &types.OIDCClaims{
+				Email:         "user@test.com",
+				EmailVerified: true,
+			},
+		},
+		{
+			name:    "unverified email domain",
+			wantErr: true,
+			cfg: &types.OIDCConfig{
+				EmailVerifiedRequired: true,
+				AllowedDomains:        []string{"test.com"},
+				AllowedUsers:          []string{},
+				AllowedGroups:         []string{},
+			},
+			claims: &types.OIDCClaims{
+				Email:         "user@test.com",
+				EmailVerified: false,
+			},
+		},
+		{
+			name:    "group member",
+			wantErr: false,
+			cfg: &types.OIDCConfig{
+				EmailVerifiedRequired: true,
+				AllowedDomains:        []string{},
+				AllowedUsers:          []string{},
+				AllowedGroups:         []string{"test"},
+			},
+			claims: &types.OIDCClaims{Groups: []string{"test"}},
+		},
+		{
+			name:    "non group member",
+			wantErr: true,
+			cfg: &types.OIDCConfig{
+				EmailVerifiedRequired: true,
+				AllowedDomains:        []string{},
+				AllowedUsers:          []string{},
+				AllowedGroups:         []string{"nope"},
+			},
+			claims: &types.OIDCClaims{Groups: []string{"testo"}},
+		},
+		{
+			name:    "group member but bad domain",
+			wantErr: true,
+			cfg: &types.OIDCConfig{
+				EmailVerifiedRequired: true,
+				AllowedDomains:        []string{"user@good.com"},
+				AllowedUsers:          []string{},
+				AllowedGroups:         []string{"test group"},
+			},
+			claims: &types.OIDCClaims{Groups: []string{"test group"}, Email: "bad@bad.com", EmailVerified: true},
+		},
+		{
+			name:    "all checks pass",
+			wantErr: false,
+			cfg: &types.OIDCConfig{
+				EmailVerifiedRequired: true,
+				AllowedDomains:        []string{"test.com"},
+				AllowedUsers:          []string{"user@test.com"},
+				AllowedGroups:         []string{"test group"},
+			},
+			claims: &types.OIDCClaims{Groups: []string{"test group"}, Email: "user@test.com", EmailVerified: true},
+		},
+		{
+			name:    "all checks pass with unverified email",
+			wantErr: false,
+			cfg: &types.OIDCConfig{
+				EmailVerifiedRequired: false,
+				AllowedDomains:        []string{"test.com"},
+				AllowedUsers:          []string{"user@test.com"},
+				AllowedGroups:         []string{"test group"},
+			},
+			claims: &types.OIDCClaims{Groups: []string{"test group"}, Email: "user@test.com", EmailVerified: false},
+		},
+		{
+			name:    "fail on unverified email",
+			wantErr: true,
+			cfg: &types.OIDCConfig{
+				EmailVerifiedRequired: true,
+				AllowedDomains:        []string{"test.com"},
+				AllowedUsers:          []string{"user@test.com"},
+				AllowedGroups:         []string{"test group"},
+			},
+			claims: &types.OIDCClaims{Groups: []string{"test group"}, Email: "user@test.com", EmailVerified: false},
+		},
+	}
+
+	for _, tC := range testCases {
+		t.Run(tC.name, func(t *testing.T) {
+			if err := doOIDCAuthorization(tC.cfg, tC.claims); ((err != nil) && !tC.wantErr) || ((err == nil) && tC.wantErr) {
+				t.Errorf("bad authorization: %s > want=%v | got=%v", tC.name, tC.wantErr, err)
+			}
+		})
+	}
+}
