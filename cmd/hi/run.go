fde054dbbabba208acb2cd934ab0146585ab9dc0
Kristoffer Dalby <kristoffer@dalby.cc>
2026-02-16 14:16:52 +0000

    all: upgrade from Go 1.26rc2 to Go 1.26.0

--- cmd/hi/run.go
+++ cmd/hi/run.go
@@ -76,7 +76,7 @@ func detectGoVersion() string {
 
 	content, err := os.ReadFile(goModPath)
 	if err != nil {
-		return "1.26rc2"
+		return "1.26.0"
 	}
 
 	lines := splitLines(string(content))
@@ -91,7 +91,7 @@ func detectGoVersion() string {
 		}
 	}
 
-	return "1.26rc2"
+	return "1.26.0"
 }
 
 // splitLines splits a string into lines without using strings.Split.
