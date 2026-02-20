72a6601daecfbea5ab71ff578d2ddc2d77441b6b
Florian Preinstorfer <site-github@nblock.org>
2026-02-20 21:09:25 +0100

    Switch to mdformat to format docs
    - Use mdformat and mdformat-mkdocs to format docs
    - Add mdformat to Makefile and pre-commit-config
    - Prettier ignores docs/

--- flake.nix
+++ flake.nix
@@ -179,6 +179,10 @@
             yq-go
             ripgrep
             postgresql
+            python313Packages.mdformat
+            python313Packages.mdformat-footnote
+            python313Packages.mdformat-frontmatter
+            python313Packages.mdformat-mkdocs
             prek
 
             # 'dot' is needed for pprof graphs
